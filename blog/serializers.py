4aff228e0bdfabf9bf6011b0d95c39fa9d493f02
Aaryan Mehta <aaryanmta@gmail.com>
2024-02-29 10:56:01 +0530

    test.py init

--- blog/serializers.py
+++ blog/serializers.py
@@ -12,25 +12,15 @@ class BlogSerializer(serializers.ModelSerializer):
         fields = ('title', 'text', 'author', 'id')
 
     def validate_author(self, value):
-        """
-        Check if user exists in database and return User instance.
-        """
         try:
             return User.objects.get(username=value)
         except ObjectDoesNotExist:
             raise ValidationError("User with username {} does not exist.".format(value))
 
     def create(self, validated_data):
-        """
-        Create and return a new `Blog` instance, given the validated data.
-        """
-
         return Blog.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `Blog` instance, given the validated data.
-        """
         instance.title = validated_data.get('title', instance.title)
         instance.text = validated_data.get('text', instance.text)
 
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+from .models import Blog
+
+class BlogCreateTestCase(APITestCase):
+    def test_create_blog(self):
+        pass
